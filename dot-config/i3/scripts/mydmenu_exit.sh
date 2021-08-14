a388215f4d9ba6129e6360de0cc63bf0037da46d
desdrianton <desdrianton@gmail.com>
2021-08-14 10:14:45 +0700

    Make a perfection

--- dot-config/i3/scripts/mydmenu_exit.sh
+++ dot-config/i3/scripts/mydmenu_exit.sh
@@ -1,6 +1,6 @@
 while [ "$select" != "CANCEL" -a "$select" != "LOGOUT" -a "$select" != "POWEROFF" -a "$select" != "REBOOT" ];
 do
-	select=$(echo 'LOGOUT\nPOWEROFF\nREBOOT' | dmenu -nb '#000000' -nf '#00FF00' -sb '#FF0000' -sf '#000000' -fn 'Xirod-10:regular' -i -p "What do you want? ")
+	select=$(echo 'LOGOUT\nREBOOT\nPOWEROFF' | dmenu -nb '#000000' -nf '#00FF00' -sb '#FF0000' -sf '#000000' -fn 'Xirod-10:regular' -i -p "What do you want? ")
 	[ -z "$select" ] && exit 0
 done
 [ "$select" = "LOGOUT" ] && i3-msg exit
