e46d77f07b61145f5fcbfc61097f3cdd2f9305d5
desdrianton <desdrianton@gmail.com>
2022-03-26 07:15:26 +0700

    Add suspend/sleep capability on i3

--- dot-config/i3/scripts/mydmenu_exit.sh
+++ dot-config/i3/scripts/mydmenu_exit.sh
@@ -1,8 +1,9 @@
-while [ "$select" != "CANCEL" -a "$select" != "LOGOUT" -a "$select" != "POWEROFF" -a "$select" != "REBOOT" ];
+while [ "$select" != "CANCEL" -a "$select" != "LOGOUT" -a "$select" != "POWEROFF" -a "$select" != "REBOOT" -a "$select" != "SUSPEND" ];
 do
-	select=$(echo 'LOGOUT\nREBOOT\nPOWEROFF' | dmenu -nb '#000000' -nf '#00FF00' -sb '#FF0000' -sf '#000000' -fn 'Xirod-10:regular' -i -p "What do you want? ")
+	select=$(echo 'LOGOUT\nSUSPEND\nREBOOT\nPOWEROFF' | dmenu -nb '#000000' -nf '#00FF00' -sb '#FF0000' -sf '#000000' -fn 'Xirod-10:regular' -i -p "What do you want? ")
 	[ -z "$select" ] && exit 0
 done
 [ "$select" = "LOGOUT" ] && i3-msg exit
+[ "$select" = "SUSPEND" ] && systemctl suspend
 [ "$select" = "POWEROFF" ] && shutdown now
 [ "$select" = "REBOOT" ] && reboot
